8cd9c1f8509548b505d52c41c85b2c050768fd14
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2018-11-22 20:40:30 +0800

    Initial Commit

---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by freakytune on 2018/11/22.
+//  Copyright © 2018 freakytune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
