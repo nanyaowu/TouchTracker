8cd74058943a05a4d2af9f4e4958b7241fff679b
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2018-11-23 18:12:50 +0800

    畫直線

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by freakytune on 2018/11/22.
+//  Copyright © 2018 freakytune. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
